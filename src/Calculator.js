522a7cdcb6a367fb4fa331a4a74782564d91a159
Mohamed CHADAD <chadad.mohamed@gmail.com>
2019-10-11 16:43:16 +0100

    ReactDoc: Main Concepts - Lifting State Up (part II)

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
 
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        this.handleChange = this.handleChange.bind(this);
         this.state = {temperature: ''};
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(e) {
-        console.log(e);
+    handleChange(e, unit) {
         this.setState({
-            temperature: e.target.value
+            temperature: e,
+            scale: unit
         })
     }
 
     render() {
+        const scale = this.state.scale;
         const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
         return (
-            <fieldset>
-                <legend>Enter temperature in C° :</legend>
-                <input
-                    value={temperature}
-                    onChange={this.handleChange}
-                />
-                <BoilingVerdict
-                    celsuis={parseFloat(temperature)}
-                />
-            </fieldset>
+            <div>
+                <TemperatureInput temperature={celsius} scale={'c'} onChange={(e) => this.handleChange(e, 'c')} />
+                <TemperatureInput temperature={fahrenheit} scale={'f'}  onChange={(e) => this.handleChange(e, 'f')} />
+            </div>
         )
     }
 }
